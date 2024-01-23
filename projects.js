55568e90ba5c536db8574aae39ada97826d6154f
Claudio <cfanzini@gmail.com>
2024-01-23 22:07:03 +0100

    Upload the projects page

---
+++ projects.js
@@ -0,0 +1,89 @@
+// Smeat Project
+let singleProjectH1Smeat = document.querySelector('#smeat h1')
+let singleDescriptionSmeat = document.querySelector('#smeat .description')
+let singleProgramsSmeat = document.querySelector('#smeat .programs')
+
+singleProjectH1Smeat.addEventListener("click", () => {
+    if (!singleDescriptionSmeat.classList.contains('disappear') && !singleProgramsSmeat.classList.contains('disappear')) {
+        singleDescriptionSmeat.classList.add('disappear')
+        singleProgramsSmeat.classList.add('disappear')
+    } else {
+        singleDescriptionSmeat.classList.remove('disappear')
+        singleProgramsSmeat.classList.remove('disappear')
+}
+})
+
+// Exchange Project
+let singleProjectH1Exchange = document.querySelector('#exchange h1')
+let singleDescriptionExchange = document.querySelector('#exchange .description')
+let singleProgramsExchange = document.querySelector('#exchange .programs')
+
+singleProjectH1Exchange.addEventListener("click", () => {
+    if (!singleDescriptionExchange.classList.contains('disappear') && !singleProgramsExchange.classList.contains('disappear')) {
+        singleDescriptionExchange.classList.add('disappear')
+        singleProgramsExchange.classList.add('disappear')
+    } else {
+        singleDescriptionExchange.classList.remove('disappear')
+        singleProgramsExchange.classList.remove('disappear')
+}
+})
+
+// Bitshop Project
+let singleProjectH1Bitshop = document.querySelector('#bitshop h1')
+let singleDescriptionBitshop = document.querySelector('#bitshop .description')
+let singleProgramsBitshop = document.querySelector('#bitshop .programs')
+
+singleProjectH1Bitshop.addEventListener("click", () => {
+    if (!singleDescriptionBitshop.classList.contains('disappear') && !singleProgramsBitshop.classList.contains('disappear')) {
+        singleDescriptionBitshop.classList.add('disappear')
+        singleProgramsBitshop.classList.add('disappear')
+    } else {
+        singleDescriptionBitshop.classList.remove('disappear')
+        singleProgramsBitshop.classList.remove('disappear')
+}
+})
+
+// Fewsteps Project
+let singleProjectH1Fewsteps = document.querySelector('#fewsteps h1')
+let singleDescriptionFewsteps = document.querySelector('#fewsteps .description')
+let singleProgramsFewsteps = document.querySelector('#fewsteps .programs')
+
+singleProjectH1Fewsteps.addEventListener("click", () => {
+    if (!singleDescriptionFewsteps.classList.contains('disappear') && !singleProgramsFewsteps.classList.contains('disappear')) {
+        singleDescriptionFewsteps.classList.add('disappear')
+        singleProgramsFewsteps.classList.add('disappear')
+    } else {
+        singleDescriptionFewsteps.classList.remove('disappear')
+        singleProgramsFewsteps.classList.remove('disappear')
+}
+})
+
+// Portfolio Project
+let singleProjectH1Portfolio = document.querySelector('#portfolio h1')
+let singleDescriptionPortfolio = document.querySelector('#portfolio .description')
+let singleProgramsPortfolio = document.querySelector('#portfolio .programs')
+
+singleProjectH1Portfolio.addEventListener("click", () => {
+    if (!singleDescriptionPortfolio.classList.contains('disappear') && !singleProgramsPortfolio.classList.contains('disappear')) {
+        singleDescriptionPortfolio.classList.add('disappear')
+        singleProgramsPortfolio.classList.add('disappear')
+    } else {
+        singleDescriptionPortfolio.classList.remove('disappear')
+        singleProgramsPortfolio.classList.remove('disappear')
+}
+})
+
+// Counter Project
+let singleProjectH1Counter = document.querySelector('#counter h1')
+let singleDescriptionCounter = document.querySelector('#counter .description')
+let singleProgramsCounter = document.querySelector('#counter .programs')
+
+singleProjectH1Counter.addEventListener("click", () => {
+    if (!singleDescriptionCounter.classList.contains('disappear') && !singleProgramsCounter.classList.contains('disappear')) {
+        singleDescriptionCounter.classList.add('disappear')
+        singleProgramsCounter.classList.add('disappear')
+    } else {
+        singleDescriptionCounter.classList.remove('disappear')
+        singleProgramsCounter.classList.remove('disappear')
+}
+})
