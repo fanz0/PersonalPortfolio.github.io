d35f8a9720705661f6a2913eaa208f519373d9d3
Claudio <cfanzini@gmail.com>
2024-01-30 17:58:46 +0100

    Upload skills section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-let jsImage = document.getElementById("js-image")
-let jsParagraph = document.getElementById("js-p")
-
-jsImage.addEventListener("mouseover", () => {
-    jsParagraph.innerHTML = "Work in progress"
-})
-
-jsImage.addEventListener("mouseout", () => {
-    jsParagraph.innerHTML = "JavaScript"
-})
-
 let frontEndTag = document.getElementById("front-end-tag")
 
 frontEndTag.addEventListener("mouseover", () => {
