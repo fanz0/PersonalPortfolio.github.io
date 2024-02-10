ee518325d72958ed16ba42c632004277c59d0f16
Claudio <cfanzini@gmail.com>
2024-02-10 11:17:50 +0100

    update on the projects page

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,104 +1,83 @@
 // Smeat Project
-let singleProjectH3Smeat = document.querySelector('#smeat h3')
+let singleProjectH3Smeat = document.querySelector('#smeat .title-programs h3')
 let singleDescriptionSmeat = document.querySelector('#smeat .description')
-let singleProgramsSmeat = document.querySelector('#smeat .programs')
 
 singleProjectH3Smeat.addEventListener("click", () => {
-    if (!singleDescriptionSmeat.classList.contains('disappear') && !singleProgramsSmeat.classList.contains('disappear')) {
+    if (!singleDescriptionSmeat.classList.contains('disappear')) {
         singleDescriptionSmeat.classList.add('disappear')
-        singleProgramsSmeat.classList.add('disappear')
     } else {
         singleDescriptionSmeat.classList.remove('disappear')
-        singleProgramsSmeat.classList.remove('disappear')
 }
 })
 
 // Exchange Project
-let singleProjectH3Exchange = document.querySelector('#exchange h3')
+let singleProjectH3Exchange = document.querySelector('#exchange .title-programs h3')
 let singleDescriptionExchange = document.querySelector('#exchange .description')
-let singleProgramsExchange = document.querySelector('#exchange .programs')
 
 singleProjectH3Exchange.addEventListener("click", () => {
-    if (!singleDescriptionExchange.classList.contains('disappear') && !singleProgramsExchange.classList.contains('disappear')) {
+    if (!singleDescriptionExchange.classList.contains('disappear')) {
         singleDescriptionExchange.classList.add('disappear')
-        singleProgramsExchange.classList.add('disappear')
     } else {
         singleDescriptionExchange.classList.remove('disappear')
-        singleProgramsExchange.classList.remove('disappear')
 }
 })
 
 // Bitshop Project
-let singleProjectH3Bitshop = document.querySelector('#bitshop h3')
+let singleProjectH3Bitshop = document.querySelector('#bitshop .title-programs h3')
 let singleDescriptionBitshop = document.querySelector('#bitshop .description')
-let singleProgramsBitshop = document.querySelector('#bitshop .programs')
 
 singleProjectH3Bitshop.addEventListener("click", () => {
-    if (!singleDescriptionBitshop.classList.contains('disappear') && !singleProgramsBitshop.classList.contains('disappear')) {
+    if (!singleDescriptionBitshop.classList.contains('disappear')) {
         singleDescriptionBitshop.classList.add('disappear')
-        singleProgramsBitshop.classList.add('disappear')
     } else {
         singleDescriptionBitshop.classList.remove('disappear')
-        singleProgramsBitshop.classList.remove('disappear')
 }
 })
 
 // Fewsteps Project
-let singleProjectH3Fewsteps = document.querySelector('#fewsteps h3')
+let singleProjectH3Fewsteps = document.querySelector('#fewsteps .title-programs h3')
 let singleDescriptionFewsteps = document.querySelector('#fewsteps .description')
-let singleProgramsFewsteps = document.querySelector('#fewsteps .programs')
 
 singleProjectH3Fewsteps.addEventListener("click", () => {
-    if (!singleDescriptionFewsteps.classList.contains('disappear') && !singleProgramsFewsteps.classList.contains('disappear')) {
+    if (!singleDescriptionFewsteps.classList.contains('disappear')) {
         singleDescriptionFewsteps.classList.add('disappear')
-        singleProgramsFewsteps.classList.add('disappear')
     } else {
         singleDescriptionFewsteps.classList.remove('disappear')
-        singleProgramsFewsteps.classList.remove('disappear')
 }
 })
 
 // Portfolio Project
-let singleProjectH3Portfolio = document.querySelector('#portfolio h3')
+let singleProjectH3Portfolio = document.querySelector('#portfolio .title-programs h3')
 let singleDescriptionPortfolio = document.querySelector('#portfolio .description')
-let singleProgramsPortfolio = document.querySelector('#portfolio .programs')
 
 singleProjectH3Portfolio.addEventListener("click", () => {
-    if (!singleDescriptionPortfolio.classList.contains('disappear') && !singleProgramsPortfolio.classList.contains('disappear')) {
+    if (!singleDescriptionPortfolio.classList.contains('disappear')) {
         singleDescriptionPortfolio.classList.add('disappear')
-        singleProgramsPortfolio.classList.add('disappear')
     } else {
         singleDescriptionPortfolio.classList.remove('disappear')
-        singleProgramsPortfolio.classList.remove('disappear')
 }
 })
 
 // Counter Project
-let singleProjectH3Counter = document.querySelector('#counter h3')
+let singleProjectH3Counter = document.querySelector('#counter .title-programs h3')
 let singleDescriptionCounter = document.querySelector('#counter .description')
-let singleProgramsCounter = document.querySelector('#counter .programs')
 
 singleProjectH3Counter.addEventListener("click", () => {
-    if (!singleDescriptionCounter.classList.contains('disappear') && !singleProgramsCounter.classList.contains('disappear')) {
+    if (!singleDescriptionCounter.classList.contains('disappear')) {
         singleDescriptionCounter.classList.add('disappear')
-        singleProgramsCounter.classList.add('disappear')
     } else {
         singleDescriptionCounter.classList.remove('disappear')
-        singleProgramsCounter.classList.remove('disappear')
 }
 })
 
 // LibraRead Project
-let singleProjectH3LibraRead = document.querySelector('#libraread h3')
+let singleProjectH3LibraRead = document.querySelector('#libraread .title-programs h3')
 let singleDescriptionLibraRead = document.querySelector('#libraread .description')
-let singleProgramsLibraRead = document.querySelector('#libraread .programs')
 
 singleProjectH3LibraRead.addEventListener("click", () => {
-    if (!singleDescriptionLibraRead.classList.contains('disappear') && !singleProgramsLibraRead.classList.contains('disappear')) {
+    if (!singleDescriptionLibraRead.classList.contains('disappear')) {
         singleDescriptionLibraRead.classList.add('disappear')
-        singleProgramsLibraRead.classList.add('disappear')
     } else {
         singleDescriptionLibraRead.classList.remove('disappear')
-        singleProgramsLibraRead.classList.remove('disappear')
 }
 })
