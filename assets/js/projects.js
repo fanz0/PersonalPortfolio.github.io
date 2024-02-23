e3588f337a9492ea3f7ea8729714c211c411c3e6
Claudio <cfanzini@gmail.com>
2024-02-23 14:17:48 +0100

    upload skills and projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,107 +1,149 @@
 // Smeat Project
-let singleProjectH3Smeat = document.querySelector('#smeat .title-programs h3')
-let singleDescriptionSmeat = document.querySelector('#smeat .description')
+let singleProjectH3Smeat = document.querySelector("#smeat .title-programs h3");
+let singleDescriptionSmeat = document.querySelector("#smeat .description");
 
 singleProjectH3Smeat.addEventListener("click", () => {
-    if (!singleDescriptionSmeat.classList.contains('disappear')) {
-        singleDescriptionSmeat.classList.add('disappear')
-    } else {
-        singleDescriptionSmeat.classList.remove('disappear')
-}
-})
+  if (!singleDescriptionSmeat.classList.contains("disappear")) {
+    singleDescriptionSmeat.classList.add("disappear");
+  } else {
+    singleDescriptionSmeat.classList.remove("disappear");
+  }
+});
 
 // Exchange Project
-let singleProjectH3Exchange = document.querySelector('#exchange .title-programs h3')
-let singleDescriptionExchange = document.querySelector('#exchange .description')
+let singleProjectH3Exchange = document.querySelector(
+  "#exchange .title-programs h3"
+);
+let singleDescriptionExchange = document.querySelector(
+  "#exchange .description"
+);
 
 singleProjectH3Exchange.addEventListener("click", () => {
-    if (!singleDescriptionExchange.classList.contains('disappear')) {
-        singleDescriptionExchange.classList.add('disappear')
-    } else {
-        singleDescriptionExchange.classList.remove('disappear')
-}
-})
+  if (!singleDescriptionExchange.classList.contains("disappear")) {
+    singleDescriptionExchange.classList.add("disappear");
+  } else {
+    singleDescriptionExchange.classList.remove("disappear");
+  }
+});
 
 // Bitshop Project
-let singleProjectH3Bitshop = document.querySelector('#bitshop .title-programs h3')
-let singleDescriptionBitshop = document.querySelector('#bitshop .description')
+let singleProjectH3Bitshop = document.querySelector(
+  "#bitshop .title-programs h3"
+);
+let singleDescriptionBitshop = document.querySelector("#bitshop .description");
 
 singleProjectH3Bitshop.addEventListener("click", () => {
-    if (!singleDescriptionBitshop.classList.contains('disappear')) {
-        singleDescriptionBitshop.classList.add('disappear')
-    } else {
-        singleDescriptionBitshop.classList.remove('disappear')
-}
-})
+  if (!singleDescriptionBitshop.classList.contains("disappear")) {
+    singleDescriptionBitshop.classList.add("disappear");
+  } else {
+    singleDescriptionBitshop.classList.remove("disappear");
+  }
+});
 
 // Fewsteps Project
-let singleProjectH3Fewsteps = document.querySelector('#fewsteps .title-programs h3')
-let singleDescriptionFewsteps = document.querySelector('#fewsteps .description')
+let singleProjectH3Fewsteps = document.querySelector(
+  "#fewsteps .title-programs h3"
+);
+let singleDescriptionFewsteps = document.querySelector(
+  "#fewsteps .description"
+);
 
 singleProjectH3Fewsteps.addEventListener("click", () => {
-    if (!singleDescriptionFewsteps.classList.contains('disappear')) {
-        singleDescriptionFewsteps.classList.add('disappear')
-    } else {
-        singleDescriptionFewsteps.classList.remove('disappear')
-}
-})
+  if (!singleDescriptionFewsteps.classList.contains("disappear")) {
+    singleDescriptionFewsteps.classList.add("disappear");
+  } else {
+    singleDescriptionFewsteps.classList.remove("disappear");
+  }
+});
 
 // Portfolio Project
-let singleProjectH3Portfolio = document.querySelector('#portfolio .title-programs h3')
-let singleDescriptionPortfolio = document.querySelector('#portfolio .description')
+let singleProjectH3Portfolio = document.querySelector(
+  "#portfolio .title-programs h3"
+);
+let singleDescriptionPortfolio = document.querySelector(
+  "#portfolio .description"
+);
 
 singleProjectH3Portfolio.addEventListener("click", () => {
-    if (!singleDescriptionPortfolio.classList.contains('disappear')) {
-        singleDescriptionPortfolio.classList.add('disappear')
-    } else {
-        singleDescriptionPortfolio.classList.remove('disappear')
-}
-})
+  if (!singleDescriptionPortfolio.classList.contains("disappear")) {
+    singleDescriptionPortfolio.classList.add("disappear");
+  } else {
+    singleDescriptionPortfolio.classList.remove("disappear");
+  }
+});
 
 // Counter Project
-let singleProjectH3Counter = document.querySelector('#counter .title-programs h3')
-let singleDescriptionCounter = document.querySelector('#counter .description')
+let singleProjectH3Counter = document.querySelector(
+  "#counter .title-programs h3"
+);
+let singleDescriptionCounter = document.querySelector("#counter .description");
 
 singleProjectH3Counter.addEventListener("click", () => {
-    if (!singleDescriptionCounter.classList.contains('disappear')) {
-        singleDescriptionCounter.classList.add('disappear')
-    } else {
-        singleDescriptionCounter.classList.remove('disappear')
-}
-})
+  if (!singleDescriptionCounter.classList.contains("disappear")) {
+    singleDescriptionCounter.classList.add("disappear");
+  } else {
+    singleDescriptionCounter.classList.remove("disappear");
+  }
+});
 
 // LibraRead Project
-let singleProjectH3LibraRead = document.querySelector('#libraread .title-programs h3')
-let singleDescriptionLibraRead = document.querySelector('#libraread .description')
+let singleProjectH3LibraRead = document.querySelector(
+  "#libraread .title-programs h3"
+);
+let singleDescriptionLibraRead = document.querySelector(
+  "#libraread .description"
+);
 
 singleProjectH3LibraRead.addEventListener("click", () => {
-    if (!singleDescriptionLibraRead.classList.contains('disappear')) {
-        singleDescriptionLibraRead.classList.add('disappear')
-    } else {
-        singleDescriptionLibraRead.classList.remove('disappear')
-}
-})
+  if (!singleDescriptionLibraRead.classList.contains("disappear")) {
+    singleDescriptionLibraRead.classList.add("disappear");
+  } else {
+    singleDescriptionLibraRead.classList.remove("disappear");
+  }
+});
 
 // PeasOfMind Project
-let singleProjectH3PeasOfMind = document.querySelector('#peasofmind .title-programs h3')
-let singleDescriptionPeasOfMind = document.querySelector('#peasofmind .description')
+let singleProjectH3PeasOfMind = document.querySelector(
+  "#peasofmind .title-programs h3"
+);
+let singleDescriptionPeasOfMind = document.querySelector(
+  "#peasofmind .description"
+);
 
 singleProjectH3PeasOfMind.addEventListener("click", () => {
-    if (!singleDescriptionPeasOfMind.classList.contains('disappear')) {
-        singleDescriptionPeasOfMind.classList.add('disappear')
-    } else {
-        singleDescriptionPeasOfMind.classList.remove('disappear')
-}
-})
+  if (!singleDescriptionPeasOfMind.classList.contains("disappear")) {
+    singleDescriptionPeasOfMind.classList.add("disappear");
+  } else {
+    singleDescriptionPeasOfMind.classList.remove("disappear");
+  }
+});
 
 // FlightFootprintTracker Project
-let singleProjectH3FlightFootprintTracker = document.querySelector('#flightfootprinttracker .title-programs h3')
-let singleDescriptionFlightFootprintTracker = document.querySelector('#flightfootprinttracker .description')
+let singleProjectH3FlightFootprintTracker = document.querySelector(
+  "#flightfootprinttracker .title-programs h3"
+);
+let singleDescriptionFlightFootprintTracker = document.querySelector(
+  "#flightfootprinttracker .description"
+);
 
 singleProjectH3FlightFootprintTracker.addEventListener("click", () => {
-    if (!singleDescriptionFlightFootprintTracker.classList.contains('disappear')) {
-        singleDescriptionFlightFootprintTracker.classList.add('disappear')
-    } else {
-        singleDescriptionFlightFootprintTracker.classList.remove('disappear')
-}
-})
+  if (
+    !singleDescriptionFlightFootprintTracker.classList.contains("disappear")
+  ) {
+    singleDescriptionFlightFootprintTracker.classList.add("disappear");
+  } else {
+    singleDescriptionFlightFootprintTracker.classList.remove("disappear");
+  }
+});
+
+// PlantyOfFood Project
+let singleProjectH3Pof = document.querySelector("#pof .title-programs h3");
+let singleDescriptionPof = document.querySelector("#pof .description");
+
+singleProjectH3Pof.addEventListener("click", () => {
+  if (!singleDescriptionPof.classList.contains("disappear")) {
+    singleDescriptionPof.classList.add("disappear");
+  } else {
+    singleDescriptionPof.classList.remove("disappear");
+  }
+});
