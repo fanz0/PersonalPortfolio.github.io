678385b9c637830dbf87923b72354eae16a07929
Claudio <cfanzini@gmail.com>
2024-02-25 12:28:23 +0100

    last updates

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-let frontEndTag = document.getElementById("front-end-tag")
-
-frontEndTag.addEventListener("mouseover", () => {
-    frontEndTag.innerHTML = "Work in progress"
-})
-
-frontEndTag.addEventListener("mouseout", () => {
-    frontEndTag.innerHTML = "Front End Developer"
-})
